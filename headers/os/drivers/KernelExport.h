9e4110e2927b5ebb069860d72630e2036837417d
Ithamar R. Adema <ithamar@upgrade-android.com>
2015-02-21 19:08:23 +0100

    KernelExport.h: don't export dprintf for host-only
    dprintf conflicts with normal posix calls, and we still have
    debug_pritnf anyway....

--- headers/os/drivers/KernelExport.h
+++ headers/os/drivers/KernelExport.h
@@ -196,7 +196,9 @@ extern area_id		map_physical_memory(const char *areaName,
 						uint32 protection, void **_mappedAddress);
 
 /* kernel debugging facilities */
+#ifndef HAIKU_HOST_BUILD_ONLY
 extern void			dprintf(const char *format, ...) _PRINTFLIKE(1, 2);
+#endif
 extern void			dvprintf(const char *format, va_list args);
 extern void			kprintf(const char *fmt, ...) _PRINTFLIKE(1, 2);
 
