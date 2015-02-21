447d15108105a28e0bea85da4dad9a91bba32ee4
Ithamar R. Adema <ithamar@upgrade-android.com>
2015-02-21 17:37:18 +0100

    fix warnings on MacOS X 10.10+
    This fixes very repetitive warnings about symbols being redefined
    when building with the 10.10 SDK or later.

--- headers/build/host/darwin/fcntl.h
+++ headers/build/host/darwin/fcntl.h
@@ -11,16 +11,20 @@
  * to determine the target of relative file paths in the openat() and
  * similar syscalls.
  */
+#ifndef AT_FDCWD
 #define AT_FDCWD			-100	/* CWD FD for the *at() functions */
+#endif
 
 /*
  * Miscellaneous flags for the *at() syscalls.
  */
+#ifndef AT_EACCESS
 #define AT_EACCESS			0x100	/* faccessat() */
 #define AT_SYMLINK_NOFOLLOW	0x200	/* fstatat(), fchmodat(), fchownat(),
 									   utimensat() */
 #define AT_SYMLINK_FOLLOW	0x400	/* linkat() */
 #define AT_REMOVEDIR		0x800	/* unlinkat() */
+#endif
 
 __BEGIN_DECLS
 
